257b8f701fce268a8503e858b87b021bdcd9c4d4
eq96 <edquiroga96@gmail.com>
2023-08-06 21:36:22 -0500

    Completed the exercise

---
+++ 02_repeatString/repeatString.js
@@ -0,0 +1,15 @@
+const number = Math.floor(Math.random() * 20);
+
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
+};
+
+const repeatedString = repeatString("", number);
+console.log(repeatedString);
+// Do not edit below this line
+module.exports = repeatString;
